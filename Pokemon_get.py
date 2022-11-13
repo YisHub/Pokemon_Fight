1f1d1dfa7132115b99575c96413288db1289e785
jesus14sf <jesus14sf@gmail.com>
2022-11-13 18:13:54 -0300

    Add files

---
+++ Pokemon_get.py
@@ -0,0 +1,68 @@
+import pickle
+
+
+from requests_html import HTMLSession
+
+
+pokemon_base = {
+    "name": "",
+    "current_health": 100,
+    "base_health": 100,
+    "level": 1,
+    "type": None,
+    "current_exp": 0
+}
+
+
+UTL_BASE = "https://pokexperto.net/index2.php?seccion=nds/nationaldex/movimientos_nivel&pk="
+
+
+def get_pokemon(index):
+
+    url = "{}{}".format(UTL_BASE, index)
+    session = HTMLSession()
+
+    new_pokemon = pokemon_base.copy()
+    pokemon_page = session.get(url)
+
+    new_pokemon["name"] = pokemon_page.html.find('.mini', first=True).text.split('\n')[0]
+
+    new_pokemon["type"] = []
+
+    for img in pokemon_page.html.find(".pkmain", first=True).find(".bordeambos", first=True).find("img"):
+        new_pokemon["type"].append(img.attrs["alt"])
+
+    new_pokemon["attacks"] = []
+
+    for attack_item in pokemon_page.html.find(".pkmain")[-1].find("tr .check3"):
+        attacks = {
+            "name": attack_item.find("td", first=True).find("a", first=True).text,
+            "type": attack_item.find("td")[1].find("img", first=True).attrs["alt"],
+            "min_level": attack_item.find("th", first=True).text,
+            "damage": int(attack_item.find("td")[3].text.replace("--", "0")),
+        }
+        new_pokemon["attacks"].append(attacks)
+
+    return new_pokemon
+
+
+def get_all_pokemons():
+    try:
+        print("Cargando el archivo de pokemons...")
+        with open("pokefile.pkl", "rb") as pokefile:
+            all_pokemons = pickle.load(pokefile)
+
+    except FileNotFoundError:
+        print("Archivo no encontrado! Cargando desde internet...")
+        all_pokemons = []
+        for index in range(1, 151):
+            all_pokemons.append(get_pokemon(index))
+            print("*", end="")
+
+        with open("pokefile.pkl", "wb") as pokefile:
+            pickle.dump(all_pokemons, pokefile)
+
+        print("\n Todos los pokemos han sido descargados!")
+
+    print("Lista de pokemons cargada!")
+    return all_pokemons
